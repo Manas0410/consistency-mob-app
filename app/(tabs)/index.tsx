47174e60e4312587af4746e7a536e49f07c5422f
Manas0410 <manasshrivastava0410@gmail.com>
2025-10-20 01:40:21 +0530

    Avatar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
-import { SignOutButton } from "@/components/SignOutButton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import HabbitCompletionCard from "@/components/ui/HabbitCompletionCard";
 import HabitCard from "@/components/ui/HabitCard";
 import StreakCard from "@/components/ui/streak-card";
 import TaskProgressCard from "@/components/ui/task-progress-card";
 import WeekTaskCompletionCard from "@/components/ui/WeekTaskCompletionCard";
 import { Colors } from "@/constants/theme";
+import { usePallet } from "@/hooks/use-pallet";
 import { useTheme } from "@/hooks/use-theme";
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { Link } from "expo-router";
+import { useUser } from "@clerk/clerk-expo";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -17,43 +16,44 @@ export default function HomeScreen() {
   const colors = theme === "dark" ? Colors.dark : Colors.light;
 
   const { user } = useUser();
+  const pallet = usePallet();
   console.log(user);
   return (
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
       <SafeAreaView style={{ flex: 1 }}>
-        <View>
-          <SignedIn>
-            <Text>Hello {user?.username}</Text>
-            <SignOutButton />
-          </SignedIn>
-          <SignedOut>
-            <Link href="/(auth)/sign-in">
-              <Text>Sign in</Text>
-            </Link>
-            <Link href="/(auth)/sign-up">
-              <Text>Sign up</Text>
-            </Link>
-          </SignedOut>
-        </View>
-        <View style={styles.Headingcnt}>
-          <Ionicons
-            name="logo-octocat"
-            size={30}
-            color={theme === "dark" ? "white" : "black"}
-            style={{ marginBottom: -22 }}
-          />
-          <Text
-            style={[
-              styles.Heading,
-              { color: theme === "dark" ? "white" : "black" },
-            ]}
-          >
-            Assista AI
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 16,
+            paddingTop: 10,
+          }}
+        >
+          <Text style={[styles.Heading, { color: pallet.shade1 }]}>
+            25 Hours
           </Text>
+          <Avatar>
+            <AvatarImage
+              source={{
+                uri:
+                  user?.imageUrl ||
+                  `https://avatars.githubusercontent.com/u/99088394?v=4`,
+              }}
+            />
+            <AvatarFallback>{user?.username?.[0]}</AvatarFallback>
+          </Avatar>
         </View>
-        <Text style={{ color: colors.textSecondary, marginLeft: 10 }}>
-          Believe you can and you're halfway there.
+        <Text
+          style={{
+            color: colors.textSecondary,
+            marginLeft: 10,
+            marginBottom: 10,
+          }}
+        >
+          Believe you can and you're halfway there!
         </Text>
+
         <ScrollView
           contentContainerStyle={{ paddingBottom: 100 }}
           showsVerticalScrollIndicator={false}
@@ -89,14 +89,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "800",
     marginBottom: 0,
-    marginTop: 24,
     display: "flex",
-  },
-  Headingcnt: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 8,
-    gap: 8,
-    marginLeft: 10,
+    flexDirection: "column",
   },
 });
