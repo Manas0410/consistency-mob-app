243a31545c9906feb3169caffb4e82bc82acb309
Manas0410 <manasshrivastava0410@gmail.com>
2025-11-04 01:56:21 +0530

    habbit selector

--- app/(tabs)/habbit.tsx
+++ app/(tabs)/habbit.tsx
@@ -4,22 +4,30 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import BackHeader from "@/components/ui/back-header";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Text } from "@/components/ui/text";
 import { View } from "@/components/ui/view";
 import { defaultHabbits } from "@/dummy/defaultHabbits";
 import { usePallet } from "@/hooks/use-pallet";
-import { Brain, Plus } from "lucide-react-native";
+import { ArrowBigRight, Brain, Plus } from "lucide-react-native";
 import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Habbit = () => {
   const [selectedTab, setSelectedTab] = useState<string>("Health");
   const pallet = usePallet();
+  const [GenerateAIenabled, setGenerateAIEnabled] = useState();
 
   return (
     <SafeAreaView style={{ flex: 1, padding: 10, backgroundColor: "#fff" }}>
+      <BackHeader title="Add Habbits" />
+      <Text variant="subtitle" style={{ marginVertical: 20 }}>
+        Slect from pre defined categories
+      </Text>
       <Tabs
         defaultValue={selectedTab}
         style={{ borderRadius: 10 }}
@@ -63,16 +71,34 @@ const Habbit = () => {
         ))}
       </Tabs>
 
+      <View>
+        <Input
+          type="textarea"
+          variant="outline"
+          placeholder="enter your goal"
+        />
+        <Button
+          icon={ArrowBigRight}
+          style={{
+            backgroundColor: pallet.shade1,
+            borderRadius: 10,
+            marginVertical: 10,
+          }}
+        >
+          GENERATE HABBITS
+        </Button>
+      </View>
+      <Separator style={{ marginVertical: 16 }} />
       <Button
         icon={Brain}
         style={{
-          marginTop: 16,
           backgroundColor: pallet.shade1,
           borderRadius: 10,
         }}
       >
         GENERATE HABBITS WITH AI
       </Button>
+      <Separator style={{ marginVertical: 16 }} />
     </SafeAreaView>
   );
 };
